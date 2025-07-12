af6f51f4ed8c5d68c0ba67659d7676d20baebe69
Pedro <pedroroangelo2002@gmail.com>
2025-07-12 16:41:43 -0300

    add config stack

---
+++ lib/productsApp-stack.ts
@@ -0,0 +1,28 @@
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as cdk from "aws-cdk-lib"
+
+import { Construct } from "constructs"
+import { SourceMap } from "module"
+
+export class ProductsAppStack extends cdk.Stack{
+    readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps){
+        super(scope, id, props)
+
+        this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, 
+            "ProductsFetchFunction", {
+                runtime: lambda.Runtime.NODEJS_20_X,
+                memorySize: 512, // MB
+                functionName: "ProductsFetchFunction",
+                entry: "lambda/products/ProductsFetchFunction.ts",
+                handler: "handler", // nome do metodo de entrada
+                timeout: cdk.Duration.seconds(5),
+                bundling:{
+                    minify: true,
+                    sourceMap: false
+                },
+            })
+    }
+}
