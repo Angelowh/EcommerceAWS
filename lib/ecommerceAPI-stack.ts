7df98835411d59432f5c3a31da9cfb37d87da8e2
Pedro <pedroroangelo2002@gmail.com>
2025-08-23 14:07:52 -0300

    add products resources methods

--- lib/ecommerceAPI-stack.ts
+++ lib/ecommerceAPI-stack.ts
@@ -7,6 +7,7 @@ import { Construct } from "constructs"
 
 export interface ECommerceApiStackProps extends cdk.StackProps{
     productsFetchHandler: lambdaNodeJS.NodejsFunction
+    productsAdminHandler: lambdaNodeJS.NodejsFunction
 }
 
 export class ECommerceApiStack extends cdk.Stack{
@@ -34,9 +35,23 @@ export class ECommerceApiStack extends cdk.Stack{
             })
 
             const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+            const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
 
-            // "/prodcuts"
+            // "/products"
             const productsResource = api.root.addResource("produtcs")
             productsResource.addMethod("GET", productsFetchIntegration)
+
+            // GET /products/{id}
+            const productIdResource = productsResource.addResource("{id}")
+            productIdResource.addMethod("GET", productsFetchIntegration)
+
+            // POST
+            productsResource.addMethod("POST", productsAdminIntegration)
+
+            // PUT
+            productIdResource.addMethod("PUT", productsAdminIntegration)
+
+            // DELETE
+            productIdResource.addMethod("DELETE", productsAdminIntegration)
         }    
 }
