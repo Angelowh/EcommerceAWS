3cbc5b401048e64ebd427b755ec5fc9a9e4b79de
Pedro <pedroroangelo2002@gmail.com>
2025-08-26 12:14:13 -0300

    criando metodo de updateProduct

--- lambda/products/layers/nodejs/productsRepository.ts
+++ lambda/products/layers/nodejs/productsRepository.ts
@@ -65,4 +65,23 @@ export class ProductRepository {
         }
         return data.Attributes as Product
     }
+
+    async updateProduct(productId: string, product: Product): Promise<Product>{
+        const data = await this.ddbClient.update({
+            TableName: this.productsDdb,
+            Key:{
+                id: productId
+            },
+            ReturnValues: "UPDATED_NEW",
+            UpdateExpression: "set productName = :n, code = :c, price = :p, model = :m",
+            ExpressionAttributeValues:{
+                ":n": product.productName,
+                ":c": product.code,
+                ":p": product.price,
+                ":m": product.model
+            }
+        }).promise()
+        data.Attributes!.id = productId
+        return data.Attributes as Product
+    }
 }
