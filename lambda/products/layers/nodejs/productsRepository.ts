fb395272d1314f96b564a0803d766c911c24dda1
Pedro <pedroroangelo2002@gmail.com>
2025-08-26 10:28:35 -0300

    criando getById, create e delete products

--- lambda/products/layers/nodejs/productsRepository.ts
+++ lambda/products/layers/nodejs/productsRepository.ts
@@ -1,4 +1,5 @@
 import { DocumentClient } from "aws-sdk/clients/dynamodb"
+import { error } from "console";
 import { v4 as uuid } from "uuid"
 
 export interface Product{
@@ -24,5 +25,44 @@ export class ProductRepository {
         }).promise()
 
         return data.Items as Product[]
+    }
+
+    async getProductById(productId: string): Promise<Product>{
+        const data = await this.ddbClient.get({
+            TableName: this.productsDdb,
+            Key:{
+                id: productId
+            }
+        }).promise()
+
+        if(!data.Item){
+            throw new Error('Product not found')
+        }
+        return data.Item as Product
+    }
+
+    async create(product: Product): Promise<Product>{
+        product.id = uuid()
+        await this.ddbClient.put({
+            TableName: this.productsDdb,
+            Item: product
+        }).promise()
+
+        return product
+    }
+
+    async deleteProduct(productId: string): Promise<Product>{
+        const data = await this.ddbClient.delete({
+            TableName: this.productsDdb,
+            Key:{
+                id: productId
+            },
+            ReturnValues: "ALL_OLD"
+        }).promise()
+
+        if(!data.Attributes){
+            throw new Error('Product not found')
+        }
+        return data.Attributes as Product
     }
 }
