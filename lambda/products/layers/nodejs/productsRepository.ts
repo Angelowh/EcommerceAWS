aee7bd1d0c7114b769e3caedf8c99c0d6a9b9f4a
Pedro <pedroroangelo2002@gmail.com>
2025-08-26 10:06:25 -0300

    criando getAllProducts

--- lambda/products/layers/nodejs/productsRepository.ts
+++ lambda/products/layers/nodejs/productsRepository.ts
@@ -1,3 +1,5 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb"
+import { v4 as uuid } from "uuid"
 
 export interface Product{
     id: string;
@@ -5,4 +7,22 @@ export interface Product{
     code: string;
     price: number;
     model: string;
+}
+
+export class ProductRepository {
+    private ddbClient: DocumentClient
+    private productsDdb: string
+
+    constructor(ddbClient: DocumentClient, productsDdb: string){
+        this.ddbClient = ddbClient
+        this.productsDdb = productsDdb
+    }
+
+    async getAllProducts(): Promise<Product[]>{
+        const data = await this.ddbClient.scan({
+            TableName: this.productsDdb
+        }).promise()
+
+        return data.Items as Product[]
+    }
 }
